af0eb8823abba61660f439be02e562cf4564c10f
Oliver McPheely <olivermcpheely@Olivers-iMac.local>
2018-05-11 15:14:23 +0100

    intial commit

---
+++ LeetCode/src/TwoSum.java
@@ -0,0 +1,28 @@
+
+public class TwoSum {
+	public static void main(String[] args){
+		int[] nums = {2, 12, 11, 15};
+		int target = 9;
+		int[] solution = twoSum(nums, target);
+		if (solution != null){
+			for (int i = 0; i < solution.length; i++){
+				System.out.println(solution[i]);
+			}
+		} else {
+			System.out.println("Unable to find two numbers that add up to " + target);
+		}
+	}
+	
+    public static int[] twoSum(int[] nums, int target) {
+  
+    	for (int i = 0; i < nums.length; i++){
+    		for (int x = 0; x < nums.length; x++){
+    			if (nums[i] + nums[x] == target){
+    				int[] results = {nums[i], nums[x]};
+    				return results;
+    			}
+    		}
+    	}
+        return null;
+    }
+}
