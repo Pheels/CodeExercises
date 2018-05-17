6025ce5c935804135c2627b704e1426478f5befa
Oliver McPheely <o.mcpheely@gmail.com>
2018-05-17 09:18:11 +0000

    Added RemoveElement.java

---
+++ LeetCode/src/RemoveElement.java
@@ -0,0 +1,29 @@
+/* 
+* Exercise: https://leetcode.com/problems/remove-element/description/ 
+* Author: Oliver McPheely 
+* Date: 17/05/2018 
+* Github: https://github.com/Pheels 
+*/ 
+
+public class RemoveElement {
+    public static void main(String[] args){
+        int[] input =  {0,1,2,2,3,0,4,2};
+        int length = removeElement(input, 2);
+        System.out.println(length);
+    }
+    
+    public static int removeElement(int[] nums, int val) {
+        int length = 0;
+        for (int i = 0; i < nums.length; i++){
+            // if value matched, move on
+            if (nums[i] == val) {
+                continue;
+            } else {
+                // else add non value to internal list and increment pointer
+                nums[length] = nums[i];
+                length++;
+            }
+        }
+        return length;
+    }
+}
